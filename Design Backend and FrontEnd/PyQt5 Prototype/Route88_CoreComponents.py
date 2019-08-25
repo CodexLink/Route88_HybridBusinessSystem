2d19c93c235819652690f3481e1b00b80b9ccde7
Janrey Licas <janjan.licas@gmail.com>
2019-08-25 11:28:08 +0800

    [Class Inheritance Restructure] Removed Reference From Selfs
    What?
    
    What happened is instead of creating multiple self.<variable_name> for each class that we want to initiate and making long line function we have to inherit those as a subclass from a target class.
    
    For instance, before
    MainWindow(Object):
    self.ui = Ui_LoginForm()
    self.ui.setupui(self)
    
    We don't want that. What we want is not referencing that particular variable, by adding it into subclass Like.
    
    MainWindow(UI_LoginForm)
    super().__init__()
    
    self.setupUi(self)
    
    Which that makes things better.

--- Design Backend and FrontEnd/PyQt5 Prototype/Route88_CoreComponents.py
+++ Design Backend and FrontEnd/PyQt5 Prototype/Route88_CoreComponents.py
@@ -30,7 +30,7 @@ class Route88_TechnicalCore(object):
         try:
             self.MySQLDataWire = MySQL.connect(host=HostServerIP, user=SQL_UCredentials, passwd=SQL_PCredential, db=SQLDatabase_Target)
         except MySQL.OperationalError as MySQL_ErrorMessage:
-            self.Route88_LoginWindow.StatusLabel.setText("Database Error: Cannot Connect to the SQL Database. Please restart.")
+            self.StatusLabel.setText("Database Error: Cannot Connect to the SQL Database. Please restart.")
             print(MySQL_ErrorMessage)
 
     # Sets CursorType for Iteration which outputs the following CursorType
@@ -40,10 +40,10 @@ class Route88_TechnicalCore(object):
         except (Exception, MySQL.OperationalError) as CursorErrMsg:
             print(CursorErrMsg)
 
-class Route88_LoginCore(QtWidgets.QMainWindow):
+class Route88_LoginCore(Ui_Route88_LoginWindow, Route88_TechnicalCore):
     # Class Initializer, __init__
-    def __init__(self):
-        super().__init__()
+    def __init__(self, Parent=None):
+        super(Route88_LoginCore, self).__init__(Parent=Parent)
         '''
             LoginWindow is Not Initialized / Seperate as a Function for Initialization.
                 Note: The reason why we did this is because I don't things to be more complicated as it should be.
@@ -51,19 +51,19 @@ class Route88_LoginCore(QtWidgets.QMainWindow):
                 other than anything that they thought, 'what the hell is this argument that you pass?'
                 ~ All other window will be seperately initialized...
         '''
-        self.Route88_LoginWindow = Ui_Route88_LoginWindow()
-        self.Login_TechnicalCoreHandler = Route88_TechnicalCore()
+        #self.Route88_LoginWindow = Ui_Route88_LoginWindow()
+        #self.Login_TechnicalCoreHandler = Route88_TechnicalCore()
 
-        self.Route88_LoginWindow.setupUi(self)
+        self.setupUi(self)
         self.setWindowIcon(QtGui.QIcon('IcoDisplay/r_88.ico'))
 
         self.setWindowFlags(QtCore.Qt.Dialog | QtCore.Qt.WindowTitleHint | QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowMinimizeButtonHint | QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowShadeButtonHint | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.CustomizeWindowHint | QtCore.Qt.MSWindowsFixedSizeDialogHint)
         # Button Binds for Window 'Route88_LoginForm'
-        self.Route88_LoginWindow.UserAcc_Password.returnPressed.connect(self.Route88_LoginWindow.UserAcc_SubmitData.click)
-        self.Route88_LoginWindow.UserAcc_SubmitData.clicked.connect(self.LoginForm_DataSubmission)
+        self.UserAcc_Password.returnPressed.connect(self.UserAcc_SubmitData.click)
+        self.UserAcc_SubmitData.clicked.connect(self.LoginForm_DataSubmission)
         #Run The Following Functions for Initializing User Data @ Window 'Route88_LoginForm'
-        self.Login_TechnicalCoreHandler.MySQL_ConnectDatabase()
-        self.Login_TechnicalCoreHandler.MySQL_CursorSet(MySQL.cursors.DictCursor)
+        self.MySQL_ConnectDatabase()
+        self.MySQL_CursorSet(MySQL.cursors.DictCursor)
         self.LoginForm_RunFuncAfterRender("Route88_LoginForm")
     # Technical Functions
     # Load Function After UI Rendering.
@@ -78,53 +78,53 @@ class Route88_LoginCore(QtWidgets.QMainWindow):
     def LoginForm_ParseUserEnlisted(self):
         try:
             currentRow = 0
-            self.Login_TechnicalCoreHandler.MySQL_CursorSet(MySQL.cursors.DictCursor)
-            self.Login_TechnicalCoreHandler.MySQLDataWireCursor.execute("SELECT * FROM Employees")
-            UserDataTable = self.Login_TechnicalCoreHandler.MySQLDataWireCursor.fetchall()
+            self.MySQL_CursorSet(MySQL.cursors.DictCursor)
+            self.MySQLDataWireCursor.execute("SELECT * FROM Employees")
+            UserDataTable = self.MySQLDataWireCursor.fetchall()
 
             for UserData in UserDataTable:
-                self.Route88_LoginWindow.UserAcc_Enlisted.setRowCount(currentRow + 1)
-                self.Route88_LoginWindow.UserAcc_Enlisted.setItem(currentRow, 0, QtWidgets.QTableWidgetItem('{0}, {1}'.format(UserData['lname'], UserData['fname'])))
-                self.UserColumn_1 = self.Route88_LoginWindow.UserAcc_Enlisted.item(currentRow, 0)
+                self.UserAcc_Enlisted.setRowCount(currentRow + 1)
+                self.UserAcc_Enlisted.setItem(currentRow, 0, QtWidgets.QTableWidgetItem('{0}, {1}'.format(UserData['lname'], UserData['fname'])))
+                self.UserColumn_1 = self.UserAcc_Enlisted.item(currentRow, 0)
                 self.UserColumn_1.setTextAlignment(QtCore.Qt.AlignCenter)
-                self.Route88_LoginWindow.UserAcc_Enlisted.setItem(currentRow, 1, QtWidgets.QTableWidgetItem(UserData['JobPosition']))
-                self.UserColumn_2 = self.Route88_LoginWindow.UserAcc_Enlisted.item(currentRow, 1)
+                self.UserAcc_Enlisted.setItem(currentRow, 1, QtWidgets.QTableWidgetItem(UserData['JobPosition']))
+                self.UserColumn_2 = self.UserAcc_Enlisted.item(currentRow, 1)
                 self.UserColumn_2.setTextAlignment(QtCore.Qt.AlignCenter)
                 currentRow += 1
 
-            self.Route88_LoginWindow.StatusLabel.setText("Database Loaded: Ready!")
+            self.StatusLabel.setText("Database Loaded: Ready!")
 
         except MySQL.OperationalError as LoginQueryErrorMsg:
             print('MySQL.OperationalError -> {0}'.format(str(LoginQueryErrorMsg)))
-            self.Route88_LoginWindow.StatusLabel.setText("Database Error: Failed to Load User Data. Please restart the program.")
+            self.StatusLabel.setText("Database Error: Failed to Load User Data. Please restart the program.")
 
     def LoginForm_DataSubmission(self):
         try:
-            self.Login_TechnicalCoreHandler.MySQL_CursorSet(None)
-            RowIndexSelected = self.Route88_LoginWindow.UserAcc_Enlisted.selectionModel().selectedRows()
+            self.MySQL_CursorSet(None)
+            RowIndexSelected = self.UserAcc_Enlisted.selectionModel().selectedRows()
             for RowIndexQuery in sorted(RowIndexSelected):
-                QueryReturn = self.Login_TechnicalCoreHandler.MySQLDataWireCursor.execute("SELECT fname, lname FROM Employees WHERE concat(lname, ', ', fname) = %s AND password = %s", (
-                    RowIndexQuery.data(), self.Route88_LoginWindow.UserAcc_Password.text()))
+                QueryReturn = self.MySQLDataWireCursor.execute("SELECT fname, lname FROM Employees WHERE concat(lname, ', ', fname) = %s AND password = %s", (
+                    RowIndexQuery.data(), self.UserAcc_Password.text()))
                 # After query we need to check if QueryReturn contains non-zero values. If it contains non-zero we proceed. Else not...
 
                 # We need to store the credentials that is equalled to what we expect.
                 if QueryReturn:
-                    self.Route88_LoginWindow.StatusLabel.setText("Login Success: Credential Input Matched!")
+                    self.StatusLabel.setText("Login Success: Credential Input Matched!")
                     QSound.play("SysSounds/LoginSuccessNotify.wav")
                     QtTest.QTest.qWait(1500)
-                    self.Route88_LoginWindow.StatusLabel.setText("Successfully Logged in as ...".format("Unknown User..."))
+                    self.StatusLabel.setText("Successfully Logged in as ...".format("Unknown User..."))
                     QtTest.QTest.qWait(1500)
-                    self.Login_TechnicalCoreHandler.MySQLDataWire.close() # Reconnect to Anothe SQ: Usage with Specific User Parameters
+                    self.MySQLDataWire.close() # Reconnect to Anothe SQ: Usage with Specific User Parameters
                     self.close()
                     self.InventorySys_Init()
 
                 else:
-                    self.Route88_LoginWindow.StatusLabel.setText("Login Error: Credential Input Not Matched! Check your Password!")
+                    self.StatusLabel.setText("Login Error: Credential Input Not Matched! Check your Password!")
                     QSound.play("SysSounds/LoginFailedNotify.wav")
 
         except Exception as LoginSubmissionErrorMsg:
             print(LoginSubmissionErrorMsg)
-            self.Route88_LoginWindow.StatusLabel.setText(str(LoginSubmissionErrorMsg))
+            self.StatusLabel.setText(str(LoginSubmissionErrorMsg))
             QSound.play("SysSounds/LoginFailedNotify.wav")
     # Route88_LoginForm UI Window Functions - EndPoint
 
@@ -132,7 +132,7 @@ class Route88_LoginCore(QtWidgets.QMainWindow):
 
 
 
-
+'''
 class Route88_InventoryCore(QtWidgets.QMainWindow, Route88_TechnicalCore):
     def __init__(self):
         super().__init__()
@@ -163,11 +163,11 @@ class Route88_InventoryCore(QtWidgets.QMainWindow, Route88_TechnicalCore):
         self.MySQL_ConnectDatabase()
         self.MySQL_CursorSet(MySQL.cursors.DictCursor)
         self.LoginForm_RunFuncAfterRender("")
-
+'''
 
 # Literal Procedural Programming Part
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    Route88_CoreLoad = Route88_LoginCore()
-    Route88_CoreLoad.show()
+    Route88_Instance = Route88_LoginCore()
+    Route88_Instance.show()
     sys.exit(app.exec_())
